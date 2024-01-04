3c18a141979d7cc99a45f9c274aed4fbb66f8f39
akkushwaha7834 <akkushwaha018@gmail.com>
2024-01-04 23:06:04 +0530

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,20 @@ function App() {
 
   return (
     <>
-      <h1 className="main_heading">
-        Main Heading
-      </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="main_heading">
+              Main Heading
+            </h1>
+          </div>
+          <div className="col-md-6">
+            <h2 className="sub_heading">
+              Sub Heading
+            </h2>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
