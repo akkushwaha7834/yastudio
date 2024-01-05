3ea5f99dc8120fc97168e8d0b62d78518c3eec04
akkushwaha7834 <akkushwaha018@gmail.com>
2024-01-05 10:04:03 +0530

    Props added for title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">YaStudio</a>
+          <a className="navbar-brand" href="/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -28,4 +29,9 @@ export default function Navbar() {
         </div>
       </nav>
   )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string,
+    aboutText: PropTypes.string
 }
